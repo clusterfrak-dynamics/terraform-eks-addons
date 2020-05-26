94d6231ca30eb3cac647c8d27cc6f30b356ac8ae
Kevin Lefevre <lefevre.kevin@gmail.com>
2020-05-26 22:06:52 +0200

    feat: use new istio operator v1.6
    Signed-off-by: Kevin Lefevre <lefevre.kevin@gmail.com>

--- istio-operator.tf
+++ istio-operator.tf
@@ -7,12 +7,26 @@ locals {
   )
 }
 
-data "kubectl_file_documents" "istio_operator" {
+data "kubectl_path_documents" "istio_operator_crds" {
   count   = local.istio_operator["enabled"] ? 1 : 0
-  content = file("./templates/istio_operator.yaml")
+  pattern = "./templates/istio-operator/crds/*.yaml"
+}
+
+data "kubectl_path_documents" "istio_operator" {
+  count   = local.istio_operator["enabled"] ? 1 : 0
+  pattern = "./templates/istio-operator/*.yaml"
+}
+
+resource "kubectl_manifest" "istio_operator_crds" {
+  count      = local.istio_operator["enabled"] ? length(data.kubectl_path_documents.istio_operator_crds[0].documents) : 0
+  yaml_body  = element(data.kubectl_path_documents.istio_operator_crds[0].documents, count.index)
 }
 
 resource "kubectl_manifest" "istio_operator" {
-  count     = local.istio_operator["enabled"] ? length(data.kubectl_file_documents.istio_operator[0].documents) : 0
-  yaml_body = element(data.kubectl_file_documents.istio_operator[0].documents, count.index)
+  count     = local.istio_operator["enabled"] ? length(data.kubectl_path_documents.istio_operator[0].documents) : 0
+  yaml_body = element(data.kubectl_path_documents.istio_operator[0].documents, count.index)
+
+  depends_on = [
+    kubectl_manifest.istio_operator_crds
+  ]
 }
