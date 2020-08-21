edf3043cb144cb89213985e2c224adfe9cc65cbb
Kevin Lefevre <lefevre.kevin@gmail.com>
2020-08-21 16:45:01 +0200

    chore: fmt
    Signed-off-by: Kevin Lefevre <lefevre.kevin@gmail.com>

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.13"
   required_providers {
     kubectl = {
-      source = "gavinbunney/kubectl"
+      source  = "gavinbunney/kubectl"
       version = "1.6.2"
     }
   }
